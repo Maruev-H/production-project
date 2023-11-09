146d5f0090d5c263c9a12248123d142768bc075e
Maruev Muhammadhabib <123553664+Maruev-H@users.noreply.github.com>
2023-11-09 14:45:03 +0300

    13 Svg loader. File loader. Button UI kit

--- config/build/buildResolvers.ts
+++ config/build/buildResolvers.ts
@@ -1,7 +1,12 @@
 import { ResolveOptions } from "webpack";
+import { BuildOptions } from "./types/config";
 
-export function buildResolvers(): ResolveOptions {
+export function buildResolvers(optons: BuildOptions): ResolveOptions {
   return {
     extensions: [".tsx", ".ts", ".js"],
+    preferAbsolute: true,
+    modules: [optons.paths.src, "node_modules"],
+    alias: {},
+    mainFiles: ["index"],
   };
 }
---
+++ src/app/App.tsx
@@ -0,0 +1,20 @@
+import "app/styles/index.scss";
+import { Link } from "react-router-dom";
+
+import { useTheme } from "app/providers/ThemeProvider/lib/useTheme";
+import { classNames } from "shared/lib/classNames/classNames";
+import { AppRouter } from "./providers/router";
+import { Navbar } from "widgets/Navbar";
+
+const App = () => {
+  const { theme } = useTheme();
+
+  return (
+    <div className={classNames("app", {}, [theme])}>
+      <Navbar />
+      <AppRouter />
+    </div>
+  );
+};
+
+export default App;
