e60a22f5150762f3216add36681ab1e474a6d127
Amin Maruev <ma.maruev@omp.ru>
2024-02-22 15:42:31 +0300

    upgrade sidebar,  add button themes

--- src/features/LangSwitcher/ui/LangSwitcher.tsx
+++ src/features/LangSwitcher/ui/LangSwitcher.tsx
@@ -1,6 +1,6 @@
 import { useTranslation } from 'react-i18next';
 import { classNames } from 'shared/lib/classNames/classNames';
-import { Button, ThemeButton } from 'shared/ui/Button/Button';
+import { Button, ButtonTheme } from 'shared/ui/Button/Button';
 import cls from './LangSwitcher.module.scss';
 
 interface LangSwitcherProps {
@@ -15,7 +15,7 @@ export function LangSwitcher({ className }: LangSwitcherProps) {
     };
 
     return (
-        <Button theme={ThemeButton.CLEAR} onClick={toggle} className={className}>
+        <Button theme={ButtonTheme.CLEAR} onClick={toggle} className={className}>
             {t('Язык')}
         </Button>
     );
--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -2,28 +2,43 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { ButtonHTMLAttributes, FC } from 'react';
 import cls from './Button.module.scss';
 
-export enum ThemeButton {
+export enum ButtonTheme {
     CLEAR = 'clear',
     OUTLINE = 'outline',
+    BACKGROUND = 'background',
+    BACKGROUND_INVERTED = 'backgroundInverted',
 }
 
-interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>{
-    className?: string;
-    theme?: ThemeButton;
+export enum ButtonSize {
+    M = 'size_m',
+    L = 'size_l',
+    XL = 'size_xl',
+}
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+    className?: string
+    theme?: ButtonTheme
+    square?: boolean
+    size?: ButtonSize
 }
 
 export const Button: FC<ButtonProps> = (props) => {
     const {
-        className,
-        children,
-        theme,
-        ...otherProps
+        className, children, theme, square, size = ButtonSize.M, ...otherProps
     } = props;
+
+    const mods: Record<string, boolean> = {
+        [cls.square]: square,
+    };
 
     return (
         <button
             type="button"
-            className={classNames(cls.Button, { [cls[theme]]: true }, [className])}
+            className={classNames(cls.Button, mods, [
+                className,
+                cls[theme],
+                cls[size],
+            ])}
             {...otherProps}
         >
             {children}
